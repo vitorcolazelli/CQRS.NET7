29fae0f98dbc05337cb52c5836f15c25215b3888
vitorcolazelli <vitorcolazelli@gmail.com>
2023-11-07 15:42:22 -0300

    Implementando repository pattern e conexao com MongoDB

--- SM-Post/Post.CMD/Post.CMD.Api/Program.cs
+++ SM-Post/Post.CMD/Post.CMD.Api/Program.cs
@@ -1,9 +1,10 @@
+using Post.CMD.Infrastructure.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection(nameof(MongoDBConfig)));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ SM-Post/Post.CMD/Post.CMD.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,36 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Post.CMD.Infrastructure.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.CMD.Infrastructure.Repositories
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly IMongoCollection<EventModel> _eventStoreCollection;
+
+        public EventStoreRepository(IOptions<MongoDBConfig> config)
+        {
+            var mongoClient = new MongoClient(config.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(config.Value.Database);
+
+            _eventStoreCollection = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
+        }
+
+        public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+        {
+            return await _eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task SaveAsync(EventModel @event)
+        {
+            await _eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+        }
+    }
+}
